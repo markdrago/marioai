e978ae22a01298a46695872cd7ab09f6a0163124
Mark Drago <mdrago@localhost.localdomain>
2009-08-27 00:10:20 -0400

    add a few classes to kind of get started

--- src/ch/idsia/ai/agents/ai/GeneticAgent.java
+++ src/ch/idsia/ai/agents/ai/GeneticAgent.java
@@ -4,6 +4,9 @@ import ch.idsia.ai.agents.Agent;
 import ch.idsia.ai.agents.RegisterableAgent;
 import ch.idsia.mario.engine.sprites.Mario;
 import ch.idsia.mario.environments.Environment;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Number;
 
 /**
  * User: Mark Drago
@@ -35,5 +38,42 @@ public class GeneticAgent extends RegisterableAgent implements Agent {
         catch (Exception e){}
         action[Mario.KEY_SPEED] = action[Mario.KEY_JUMP] =  observation.mayMarioJump() || !observation.isMarioOnGround();
         return action;
+    }
+    
+    public class AddNode extends BinaryOpNode {
+    	public int execute(Number op1, Number op2) {
+    		return op1.intValue() + op2.intValue();
+    	}
+    }
+    
+    public abstract class BinaryOpNode implements Node {
+    	abstract int execute(Number op1, Number op2);
+    	public int get_num_arguments() { return 2; }
+    	public List<NodeArgType> get_argument_types() {
+    		ArrayList<NodeArgType> lst = new ArrayList<NodeArgType>();
+    		lst.add(NodeArgType.NUMERIC);
+    		lst.add(NodeArgType.NUMERIC);
+    		return lst;
+    	}
+    	public NodeArgType get_response_type() { return NodeArgType.INT; }
+    }
+    
+    public interface Node {
+    	int get_num_arguments();
+    	List<NodeArgType> get_argument_types();
+    	NodeArgType get_response_type();
+    	String toString();
+    }
+
+    public enum NodeArgType {
+    	INT,
+    	FLOAT,
+    	STRING,
+    	BOOLEAN,
+    	NUMERIC;
+    
+    	boolean is_numeric() {
+    		return (this == INT || this == FLOAT || this == BOOLEAN || this == NUMERIC);
+    	}
     }
 }
