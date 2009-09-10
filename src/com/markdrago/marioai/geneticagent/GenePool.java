43ea9d8640059f84cf7c53b1ff70354b69bd3cdd
Mark Drago <mdrago@laptop.(none)>
2009-09-09 22:57:36 -0400

    add mostly empty genepool class

---
+++ src/com/markdrago/marioai/geneticagent/GenePool.java
@@ -0,0 +1,22 @@
+package com.markdrago.marioai.geneticagent;
+
+import ch.idsia.ai.EA;
+
+public class GenePool implements EA {
+
+	public double[] getBestFitnesses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Breedable[] getBests() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void nextGeneration() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
